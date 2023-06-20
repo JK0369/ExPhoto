3a1c94de2e11b15037d2d1f741eee32669332f10
jake <palatable7@naver.com>
2023-06-21 01:41:33 +0900

    Add Authorization of Photos

--- ExPhoto/Service/PhotoService.swift
+++ ExPhoto/Service/PhotoService.swift
@@ -12,11 +12,40 @@ protocol PhotoService {
     var authorizationStatus: PHAuthorizationStatus { get }
     var isAuthorizationLimited: Bool { get }
     
-    func requestAuthorization()
+    func requestAuthorization(completion: @escaping (PHAuthorizationStatus) -> Void)
+    func goToSetting()
 }
 
 extension PhotoService {
     var isAuthorizationLimited: Bool {
         authorizationStatus == .limited
+    }
+    
+    func goToSetting() {
+        guard
+            let url = URL(string: UIApplication.openSettingsURLString),
+            UIApplication.shared.canOpenURL(url)
+        else { return }
+            
+        UIApplication.shared.open(url, completionHandler: nil)
+    }
+}
+
+final class PhotoServiceV1: PhotoService {
+    var authorizationStatus: PHAuthorizationStatus {
+        PHPhotoLibrary.authorizationStatus(for: .readWrite)
+    }
+    
+    func requestAuthorization(completion: @escaping (PHAuthorizationStatus) -> Void) {
+        guard authorizationStatus == .notDetermined else {
+            completion(authorizationStatus)
+            return
+        }
+        
+        PHPhotoLibrary.requestAuthorization(for: .readWrite) { status in
+            DispatchQueue.main.async {
+                completion(status)
+            }
+        }
     }
 }
